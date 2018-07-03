5415a06c50be5c4e605694299bc5230a047487ae
Ben Huang <ben@dinngo.co>
2018-07-03 18:41:54 +0800

    Exchange basic structure

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -1,9 +1,28 @@
 pragma solidity ^0.4.24;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ECRecovery.sol";
+
 /**
  * @title Dinngo
  * @dev Main exchange contract for Dinngo
  */
 contract Dinngo {
+    using SafeMath for uint256;
+    using ECRecovery for bytes32;
+
+    mapping (address => mapping (address => uint256)) private balance;
+
+    function deposit(address token, uint256 amount) public returns (bool) {
+        return true;
+    }
+
+    function withdraw(address token, uint256 amount) public returns (bool) {
+        return true;
+    }
+
+    function settle() public returns (bool) {
+        return true;
+    }
 
 }
