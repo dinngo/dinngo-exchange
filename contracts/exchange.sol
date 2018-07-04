6f3f77c5b9455ea799a0f6e7f563b2dc7c3ce4d0
Ben Huang <ben@dinngo.co>
2018-07-04 15:01:19 +0800

    implement deposit

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ECRecovery.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 
 /**
  * @title Dinngo
@@ -10,11 +11,24 @@ import "openzeppelin-solidity/contracts/ECRecovery.sol";
 contract Dinngo {
     using SafeMath for uint256;
     using ECRecovery for bytes32;
+    using SafeERC20 for ERC20;
 
     mapping (address => mapping (address => uint256)) private balance;
 
-    function deposit(address token, uint256 amount) public returns (bool) {
-        return true;
+    event Deposit(address token, address user, uint256 amount);
+
+    function deposit() public payable {
+        require(msg.value > 0);
+        balance[0][msg.sender] = balance[0][msg.sender].add(msg.value);
+        emit Deposit(0, msg.sender, msg.value);
+    }
+
+    function depositToken(address token, uint256 amount) public {
+        require(token != address(0));
+        require(amount > 0);
+        ERC20(token).safeTransferFrom(msg.sender, this, amount);
+        balance[token][msg.sender] = balance[token][msg.sender].add(amount);
+        emit Deposit(token, msg.sender, amount);
     }
 
     function withdraw(address token, uint256 amount) public returns (bool) {
