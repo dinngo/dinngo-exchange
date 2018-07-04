f3282f34d42381351aebddf8d8ccea7df97e8f6e
Ben Huang <ben@dinngo.co>
2018-07-04 15:36:08 +0800

    add the balance information in the event

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -15,13 +15,13 @@ contract Dinngo {
 
     mapping (address => mapping (address => uint256)) private balance;
 
-    event Deposit(address token, address user, uint256 amount);
-    event Withdraw(address token, address user, uint256 amount);
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
     function deposit() public payable {
         require(msg.value > 0);
         balance[0][msg.sender] = balance[0][msg.sender].add(msg.value);
-        emit Deposit(0, msg.sender, msg.value);
+        emit Deposit(0, msg.sender, msg.value, balance[0][msg.sender]);
     }
 
     function depositToken(address token, uint256 amount) public {
@@ -29,14 +29,14 @@ contract Dinngo {
         require(amount > 0);
         ERC20(token).safeTransferFrom(msg.sender, this, amount);
         balance[token][msg.sender] = balance[token][msg.sender].add(amount);
-        emit Deposit(token, msg.sender, amount);
+        emit Deposit(token, msg.sender, amount, balance[token][msg.sender]);
     }
 
     function withdraw(uint256 amount) public {
         require(amount <= balance[0][msg.sender]);
         msg.sender.transfer(amount);
         balance[0][msg.sender] = balance[0][msg.sender].sub(amount);
-        emit Withdraw(0, msg.sender, amount);
+        emit Withdraw(0, msg.sender, amount, balance[0][msg.sender]);
     }
 
     function withdrawToken(address token, uint256 amount) public {
@@ -44,7 +44,7 @@ contract Dinngo {
         require(amount > 0);
         ERC20(token).safeTransfer(msg.sender, amount);
         balance[token][msg.sender] = balance[token][msg.sender].sub(amount);
-        emit Withdraw(token, msg.sender, amount);
+        emit Withdraw(token, msg.sender, amount, balance[token][msg.sender]);
     }
 
     function settle() public returns (bool) {
