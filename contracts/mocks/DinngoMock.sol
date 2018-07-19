66089e4971b457f0dea0fce86eceabb6d2a4db79
Ben Huang <ben@dinngo.co>
2018-07-19 18:17:57 +0800

    fix mock

--- contracts/mocks/DinngoMock.sol
+++ contracts/mocks/DinngoMock.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 import "../Dinngo.sol";
 
-contract DinngoMock is Dinngo() {
+contract DinngoMock is Dinngo {
     constructor(address _dinngoWallet, address _dinngoToken)
         Dinngo(_dinngoWallet, _dinngoToken)
         public
