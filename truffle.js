e31398f2f919c6e6b08a410c901bd167ceba64b9
Ben Huang <ben@dinngo.co>
2018-11-28 17:41:11 +0800

    Add solc version

--- truffle.js
+++ truffle.js
@@ -16,6 +16,9 @@ require('babel-register')({
 });
 require('babel-polyfill');
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    compilers: {
+        solc: {
+            version: "0.4.25"
+        }
+    }
 };
