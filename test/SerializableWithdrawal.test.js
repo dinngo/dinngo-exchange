c6b6c11e1870c10ed02b91ac3f7ca9c2c6f5c1e5
Ben Huang <ben@dinngo.co>
2019-02-12 18:13:09 +0800

    Fix test/SerializableWithdrawal.test.js

--- test/SerializableWithdrawal.test.js
+++ test/SerializableWithdrawal.test.js
@@ -1,27 +1,21 @@
-import { ether } from 'openzeppelin-solidity/test/helpers/ether';
-
-const BigNumber = web3.BigNumber;
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const { constants, ether } = require('openzeppelin-test-helpers');
+const { ZERO_ADDRESS } = constants;
 
 const SerializableWithdrawalMock = artifacts.require('SerializableWithdrawalMock');
-
-require('chai')
-    .use(require('chai-bignumber')(BigNumber))
-    .should();
 
 contract('SerializableWithdrawal', function ([_, user1]) {
     beforeEach(async function () {
         this.SerializableWithdrawal = await SerializableWithdrawalMock.new();
     });
-    const userID = 11;
-    const token = 0;
-    const amount = ether(1);
-    const config = 1;
-    const fee = ether(0.001);
-    const nonce = 1;
+    const userID = '11';
+    const token = '0';
+    const amount = ether('1');
+    const config = '1';
+    const fee = ether('0.001');
+    const nonce = '1';
     const r = "0x7c951ccb051dc002650c64d267dcabfcea1a552119c900bce8d59651b4d9f51b";
     const s = "0x723ee62931c50b28134e8dfa9360ba3628a3a958b3529f355eff7541e0699f1e";
-    const v = "0x00";
+    const v = '0';
     const hash = "0xdab6521870251314f0ee238e25d5d159f5ea8e774220860db80eb4f03c0c0abb";
     const ser_hex = "0x723ee62931c50b28134e8dfa9360ba3628a3a958b3529f355eff7541e0699f1e7c951ccb051dc002650c64d267dcabfcea1a552119c900bce8d59651b4d9f51b0000000000000000000000000000000000000000000000000000038d7ea4c6800000000001010000000000000000000000000000000000000000000000000de0b6b3a764000000000000000b";
 
