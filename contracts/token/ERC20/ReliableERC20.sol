4818255228fc92b162c41528476d2f3417cac300
Ben Huang <ben@dinngo.co>
2019-02-22 14:22:00 +0800

    Use ReliableERC20 instead of SafeERC20

--- contracts/token/ERC20/ReliableERC20.sol
+++ contracts/token/ERC20/ReliableERC20.sol
@@ -4,16 +4,16 @@ import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 library ReliableERC20 {
     function reliableTransfer(IERC20 token, address to, uint256 value) internal {
-        (bool ok, bytes memory ret) = token.call(abi.encodeWithSignature("transfer(address,uint256)"));
+        (bool ok, bytes memory ret) = address(token).call(abi.encodeWithSignature("transfer(address,uint256)", to, value));
         require(ok);
-        if (ret.length() != 0)
+        if (ret.length != 0)
             require(abi.decode(ret, (bool)));
     }
 
     function reliableTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
-        (bool ok, bytes memory ret) = token.call(abi.encodeWithSignature("transferFrom(address,address,uint256)"));
+        (bool ok, bytes memory ret) = address(token).call(abi.encodeWithSignature("transferFrom(address,address,uint256)", from, to , value));
         require(ok);
-        if (ret.length() != 0)
-            requrie(abi.decode(ret, (bool)));
+        if (ret.length != 0)
+            require(abi.decode(ret, (bool)));
     }
 }
