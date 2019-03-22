d47434436017ba7451f6247fcedb88162a6b2735
Ben Huang <ben@dinngo.co>
2019-03-22 20:08:09 +0800

    Fix solc version to 0.5.6

--- contracts/mocks/ProxyMock.sol
+++ contracts/mocks/ProxyMock.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.6
+;
 
 import "../proxy/Proxy.sol";
 import "./DummyImplementation.sol";
--- contracts/mocks/TimelockUpgradableProxyMock.sol
+++ contracts/mocks/TimelockUpgradableProxyMock.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.6;
 
 import "../proxy/TimelockUpgradableProxy.sol";
 import "./DummyImplementation.sol";
